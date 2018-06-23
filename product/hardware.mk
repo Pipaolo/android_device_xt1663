fb1dc0a3bde74a6d3f55c98d7152c0076d3d0d74
Pipaolo <mastastah8975@gmailcom>
2018-06-23 16:25:25 +0800

    Added org.apache.http.legacy.jar

--- product/hardware.mk
+++ product/hardware.mk
@@ -16,6 +16,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
+PRODUCT_COPY_FILES += \
+		$(LOCAL_PATH)/prebuilt/org.apache.http.legacy.jar:system/framework/org.apache.http.legacy.jar \
+
 PRODUCT_PACKAGES += \
    libmtk_symbols \
    libstlport \
