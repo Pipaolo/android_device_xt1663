35c77b912e589f2bf24ba4482d698a33fef68025
Pipaolo <mastastah8975@gmailcom>
2018-06-28 13:01:58 +0800

    Fix typo

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,7 +16,7 @@ LOCAL_MODULE := libshim_gui
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
-/system/vendor/lib/libcam.camadapter.so ( _ZN7android16CameraParameters25KEY_SNAPSHOT_PICTURE_FLIPE )
+#/system/vendor/lib/libcam.camadapter.so ( _ZN7android16CameraParameters25KEY_SNAPSHOT_PICTURE_FLIPE )
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := camera.cpp
 LOCAL_MODULE := libshim_camera
